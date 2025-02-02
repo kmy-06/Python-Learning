6e284e2442a11fa1576ded31efd4253f99a85a7e
KMY <115706052+kmy-06@users.noreply.github.com>
2025-02-02 23:06:02 +0800

    Add files via upload

---
+++ 11 - SQLite3 and Jinja2/queries.py
@@ -0,0 +1,35 @@
+SQL_SESSIONS = """
+    select distinct ts from times
+"""
+
+
+# the ‘？’ placeholders are called parameterized values.
+
+SQL_SWIMMERS_BY_SESSION = """
+    select distinct swimmers.name, swimmers.age  
+    from times, swimmers 
+    where date(times.ts) = ? and     
+    times.swimmer_id = swimmers.id 
+    order by name
+"""
+
+# below there is a date function, that converts timestamps to YYYY-MM-DD format.
+SQL_SWIMMERS_EVENTS_BY_SESSION = """  
+    select distinct events.distance, events.stroke
+    from swimmers, events, times
+    where times.swimmer_id = swimmers.id and
+    times.event_id = events.id and
+    (swimmers.name = ? and swimmers.age = ?) and
+    date(times.ts) = ?
+"""
+
+
+SQL_CHART_DATA_BY_SWIMMER_EVENT_SESSION = """
+    select times.time
+    from swimmers, events, times
+    where (swimmers.name = ? and swimmers.age = ?) and
+    (events.distance = ? and events.stroke = ?) and 
+    swimmers.id = times.swimmer_id and
+    events.id = times.event_id and
+    date(times.ts) = ?
+"""
